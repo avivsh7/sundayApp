1a4c64214e14a9339d72da81f9c49d245c2e7d99
Aviv Shoshan <avivsh7@gmail.com>
2026-02-04 23:35:10 +0200

    feat: Containerized controller and integrated into K8s deployment.

--- requirements.sh
+++ requirements.sh
@@ -7,15 +7,18 @@ kubectl create secret generic db-credentials --from-literal=password=mysecret -n
 echo "Building Backend..."
 docker build -t sunday-backend:v1.5 ./backend
 
+echo "Building Go Controller..."
+docker build -t ethereal-controller:v1 ./etherealpod-controller
+
 echo "Building Frontend..."
 docker build -t sunday-frontend:v1.0 ./frontend
 
 echo "Applying Bundle..."
 kubectl apply -f k8s/submission.yaml
 
-echo "Launching Apps..."
-(cd etherealpod-controller && go run main.go) & 
-sleep 2
 
-echo "Starting Frontend..."
+echo "Starting Port-Forwarding..."
+kubectl port-forward svc/ethereal-backend-svc 30001:8080 -n sunday-app &
+sleep 1
 echo "Done! Access the app at: http://localhost:30002"
+echo "Backend API is reachable at: http://localhost:30001"
